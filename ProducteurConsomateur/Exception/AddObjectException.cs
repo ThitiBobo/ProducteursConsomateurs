b4c0fb33d490e658a46aaf48d60d0607deaa9905
ThitiBobo <t.delplanque92@gmail.com>
2018-04-22 15:35:18 +0200

    ajout des exceptions AddObjectException et TakeObjectException

---
+++ ProducteurConsomateur/Exception/AddObjectException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class AddObjectException : Exception
+{
+    public AddObjectException()
+    {
+    }
+
+    public AddObjectException(string message) : base(message)
+    {
+    }
+
+    public AddObjectException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ ProducteurConsomateur/Exception/TakeObjectException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+internal class TakeObjectException : Exception
+{
+    public TakeObjectException()
+    {
+    }
+
+    public TakeObjectException(string message) : base(message)
+    {
+    }
+
+    public TakeObjectException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
