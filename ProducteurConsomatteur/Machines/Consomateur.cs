0cf865a7d4c5394339edcc5c95d2104c588dd18d
ThitiBobo <t.delplanque92@gmail.com>
2018-05-01 15:40:00 +0200

    ajout des commentaires pour la classe Producteur

--- ProducteurConsomatteur/Machines/Consomateur.cs
+++ ProducteurConsomatteur/Machines/Consomateur.cs
@@ -91,6 +91,5 @@ namespace ProducteurConsomatteur
             return true;
         }
 
-
     }
 }
--- ProducteurConsomatteur/Machines/Producteur.cs
+++ ProducteurConsomatteur/Machines/Producteur.cs
@@ -7,14 +7,25 @@ using System.Text;
 
 namespace ProducteurConsomatteur
 {
+    /// <summary>
+    /// Représente une instance dérivant de la classe <c>Machine</c> pouvant 
+    /// fabriquer un objet avec un temps de fabrication comprit dans une intervalle
+    /// et le déposer en sortie dans une instance implémentant l'interface Storable.
+    /// </summary>
     public class Producteur : Machine
     {
 
         #region ATTRIBUTS
+        /// <summary>
+        /// Instance de type <c>Storable</c> utilisé comme sortie
+        /// </summary>
         private Storable _output;
         #endregion
 
         #region GETSET
+        /// <summary>
+        /// Permet de modifier le Stockage de sortie
+        /// </summary>
         public Storable Output {
             get => _output;
             set => _output = value ??
@@ -23,25 +34,48 @@ namespace ProducteurConsomatteur
         #endregion
 
         #region CONSTRUCTORS
+        /// <summary>
+        /// Permet de créer une nouvelle instance en précisant l'idantifiant, le temps minimum 
+        /// et maximum de fabrication ainsi que le stokage de sorite 
+        /// </summary>
+        /// <param name="id">Identifiant de l'instance</param>
+        /// <param name="minTime">Temps minimum de fabrication</param>
+        /// <param name="maxTime">Temps maximum de fabrication</param>
+        /// <param name="input">Stokage de sortie</param>
         public Producteur(int id, uint minTime, uint maxTime, Storable output) :
             base(id, minTime, maxTime)
         {
             Output = output;
         }
 
+        /// <summary>
+        /// Permet de créer une nouvelle instance en précisant l'identifiant, le temp de fabrication
+        /// et le stokage de sortie
+        /// </summary>
+        /// <param name="id">Identifiant de l'instance</param>
+        /// <param name="time">Temps de fabrication</param>
+        /// <param name="input">Stokage de sortie</param>
+        /// <remarks>Temps de fabrication est comprise dans l'intervalle [minTime; maxTime] 
+        /// / minTime = maxTime </remarks>
         public Producteur(int id, uint time, Storable output) :
             base(id, time)
         {
             Output = output; 
         }
 
+        /// <summary>
+        /// Permet de créer une nouvelle instance à partir d'une instance déjà existante
+        /// </summary>
+        /// <param name="copy">Instance à recopier</param>
         public Producteur(Producteur copy) : base(copy)
         {
             _output = copy._output;
         }
         #endregion
 
-
+        /// <summary>
+        /// méthode contenant le protocole de fabrication de la machine
+        /// </summary>
         protected override void OnExecute()
         {
             Work();
@@ -49,6 +83,10 @@ namespace ProducteurConsomatteur
             Console.WriteLine("Machine {0}: dépot pièce P{1} ({2})", _id, _output.GetName(), _output.Count());
         }
 
+        /// <summary>
+        /// Permet de vérifier si la machine est intégre avant le démarage
+        /// </summary>
+        /// <returns>retourne <c>true</c> si l'instance est intégre, sinon <c>false</c>.</returns>
         protected override bool IsReady()
         {
             if (_output == null)
