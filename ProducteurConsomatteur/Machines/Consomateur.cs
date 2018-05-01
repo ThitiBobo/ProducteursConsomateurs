dff59d023056a3e23a9d532aaf136909a431d306
ThitiBobo <t.delplanque92@gmail.com>
2018-05-01 12:24:29 +0200

    ajout des comentaires de la classe Consomateur (1/2)

--- ProducteurConsomatteur/Machines/Consomateur.cs
+++ ProducteurConsomatteur/Machines/Consomateur.cs
@@ -3,13 +3,24 @@ using System;
 
 namespace ProducteurConsomatteur
 {
+    /// <summary>
+    /// Représente une instance dérivant de la classe <c>Machine</c> pouvant prendre un objet 
+    /// en entrée dans une instance implémentant l'interface Storable.
+    /// Et consomme l'objet avec un temps compris dans une intervalle.
+    /// </summary>
     public class Consomateur : Machine {
 
         #region ATTRIBUTS
+        /// <summary>
+        /// Instance de type <c>Storable</c> utilisé comme entrée
+        /// </summary>
         private Storable _input;
         #endregion
 
         #region GETSET
+        /// <summary>
+        /// Permet de modifier le Stokage d'entrée
+        /// </summary>
         public Storable Input
         {
             get => _input;
@@ -19,6 +30,14 @@ namespace ProducteurConsomatteur
         #endregion
 
         #region CONSTRUCTORS
+        /// <summary>
+        /// Permet de créer une nouvelle instance en précissant l'idantifiant, le temps minimum 
+        /// et maximum de fabrication ainsi que le stokage d'entrée 
+        /// </summary>
+        /// <param name="id">Identifiant de l'instance</param>
+        /// <param name="minTime">Temps minimum de fabrication</param>
+        /// <param name="maxTime">Temps maximum de fabrication</param>
+        /// <param name="input">Stokage d'entrée</param>
         public Consomateur(int id, uint minTime, uint maxTime, Storable input) : 
             base(id, minTime, maxTime)
         {
